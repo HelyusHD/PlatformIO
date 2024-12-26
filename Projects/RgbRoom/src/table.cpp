d06759cdf5b6734d9bbbb9aa03a4ac043507c68f
HelyusHD <you@example.com>
2024-12-26 08:49:13 +0100

    changed macro to const int

--- Projects/RgbRoom/src/table.cpp
+++ Projects/RgbRoom/src/table.cpp
@@ -3,7 +3,7 @@
 #include "networkConfig.h"
 #include <FastLED.h>
 
-#define LED_COUN_01 432
+const int led_count = 432;
 
 #define LED_PIN_FL 5
 #define LED_PIN_R 4
@@ -12,22 +12,22 @@
 
 // Create an ESP32 object (network information)
 EspClient myESP32(CLIENT_01, ESP_NETWORK_PASSWORD, ESP_NETWORK_SSID);
-CRGB leds[LED_COUN_01];
-CRGB background[LED_COUN_01];
-int ledMapping[LED_COUN_01];
+CRGB leds[led_count];
+CRGB background[led_count];
+int ledMapping[led_count];
 
 void setup() {
     Serial.begin(115200); // Start the serial communication at 115200 baud rate
     FastLED.addLeds<WS2815, LED_PIN_R, GRB>(leds, 0, NUM_LEDS_R); // Erster Strang
     FastLED.addLeds<WS2815, LED_PIN_FL, GRB>(leds, NUM_LEDS_R, NUM_LEDS_FL); // Zweiter Strang
-    for(int i=0;i<LED_COUN_01;i++){
+    for(int i=0;i<led_count;i++){
         if(i<NUM_LEDS_R){
         ledMapping[i] = NUM_LEDS_R - i - 1;
         } else {
         ledMapping[i] = i;
         }
     }
-    myESP32.addStrip(leds, background, LED_COUN_01, ledMapping);
+    myESP32.addStrip(leds, background, led_count, ledMapping);
  
     // Display network information from ESP32
     myESP32.showNetworkInfo();
