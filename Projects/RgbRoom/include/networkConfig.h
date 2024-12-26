43573f4ad1d300d3898bcd2052e19bbca8da8d9f
HelyusHD <you@example.com>
2024-12-26 07:31:03 +0100

    _

---
+++ Projects/RgbRoom/include/networkConfig.h
@@ -0,0 +1,11 @@
+#ifndef NETWORK_CONFIG
+#define NETWORK_CONFIG
+    #define HOME_NETWORK_PASSWORD "85505796012028034369"
+    #define HOME_NETWORK_SSID "Andre Thullen"
+
+    #define ESP_NETWORK_PASSWORD  "21345678"
+    #define ESP_NETWORK_SSID "ESP32_Network"
+
+    #define CLIENT_01 "192.168.4.2"
+    #define CLIENT_02 "192.168.4.3"
+#endif
