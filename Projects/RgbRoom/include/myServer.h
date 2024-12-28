955df38dcd80373aae06103a2ffaa5755efec53f
HelyusHD <you@example.com>
2024-12-28 04:31:26 +0100

    server remebers picked color

--- Projects/RgbRoom/include/myServer.h
+++ Projects/RgbRoom/include/myServer.h
@@ -9,6 +9,7 @@
 #include <HTTPClient.h>
 #include <ESPmDNS.h>  // Für mDNS
 #include "webpage.h"
+#include <esp_wifi.h>
 
     class EspServer {
     private:
@@ -21,6 +22,10 @@
         const std::string homeNetworkSSID;
 
         WiFiServer server;
+
+        String lastColor = "000000";
+        std::vector<IPAddress>  connectedIpAdresses;
+        int connectedClientsCount = 0;
 
     public:
         EspServer(std::string password, std::string ssid, std::string dnsName, std::string homePassword, std::string homeSsid)
@@ -97,39 +102,105 @@
         void handleClient(){
             // Webserver nur im Heimnetzwerk bedienen
             if (WiFi.status() == WL_CONNECTED) {
+
+                int numDevices = WiFi.softAPgetStationNum();
+                if(numDevices != connectedClientsCount){
+                    std::cout << numDevices << " != " << connectedClientsCount << std::endl;
+                    remeberNewDevice();
+                }
+
                 // Client abfragen
                 WiFiClient client = server.available();
                 if (client) {
-                Serial.println("Neuer Client verbunden!");
+                    Serial.println("Neuer Client verbunden!");
 
-                // Anfrage vom Client einlesen
-                String request = client.readStringUntil('\r');
-                Serial.println("Anfrage: " + request);
-                client.flush();
+                    // Anfrage vom Client einlesen
+                    String request = client.readStringUntil('\r');
+                    Serial.println("Anfrage: " + request);
+                    client.flush();
 
-                // Anfragen verarbeiten
-                if (request.indexOf("/turnOff") != -1) {
-                    sendHtml("/turnOff", CLIENT_01);
-                    sendHtml("/turnOff", CLIENT_02);
+                    // Anfragen verarbeiten
+                    if (request.indexOf("/turnOff") != -1) {
+                        sendHtml("/turnOff", CLIENT_01);
+                        sendHtml("/turnOff", CLIENT_02);
 
-                } else if (request.indexOf("/setRgbColor?color=") != -1) {
-                    // RGB-Farbe aus der Anfrage extrahieren
-                    int colorIndex = request.indexOf("color=") + 6;
-                    String colorHex = request.substring(colorIndex, colorIndex + 6);
-                    setRgbColor(colorHex, CLIENT_01);
-                    setRgbColor(colorHex, CLIENT_02);
-                } else if (request.indexOf("/sendPulse") != -1){
-                    sendHtml("/sendPulse",CLIENT_01);
-                    sendHtml("/sendPulse",CLIENT_02);
+                    } else if (request.indexOf("/setRgbColor?color=") != -1) {
+                        // RGB-Farbe aus der Anfrage extrahieren
+                        int colorIndex = request.indexOf("color=") + 6;
+                        String colorHex = request.substring(colorIndex, colorIndex + 6);
+
+                        lastColor = colorHex;
+
+                        setRgbColor(colorHex, CLIENT_01);
+                        setRgbColor(colorHex, CLIENT_02);
+                    } else if (request.indexOf("/sendPulse") != -1){
+                        sendHtml("/sendPulse",CLIENT_01);
+                        sendHtml("/sendPulse",CLIENT_02);
+                    }
+                    // update last color
+                    String html = webpage;
+                    html.replace("$LASTCOLOR", "#" + String(lastColor)); // Inject lastColor into the webpage
+
+
+                    // HTML-Antwort senden
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-type:text/html");
+                    client.println(html);
+                    client.stop();
+                    Serial.println("Client getrennt.");
+                }
+            }
+        }
+        void remeberNewDevice() {
+            // Get detailed station info
+            wifi_sta_list_t stationList;
+            tcpip_adapter_sta_list_t adapterStationList;
+
+            esp_wifi_ap_get_sta_list(&stationList); // Get station list
+            tcpip_adapter_get_sta_list(&stationList, &adapterStationList); // Map to adapter station list
+
+            
+            // Update the count of connected clients
+            connectedClientsCount = 0;
+
+            // Loop through each connected station
+            for (int i = 0; i < adapterStationList.num; i++) {
+                tcpip_adapter_sta_info_t station = adapterStationList.sta[i];
+                IPAddress deviceIP = IPAddress((uint8_t*)&station.ip.addr);
+
+                if (deviceIP.toString() == "0.0.0.0") {
+                    Serial.printf("Device %d has no valid IP yet. Skipping.\n", i + 1);
+                    continue;
                 }
 
-                // HTML-Antwort senden
-                client.println("HTTP/1.1 200 OK");
-                client.println("Content-type:text/html");
-                client.println(webpage);
-                client.stop();
-                Serial.println("Client getrennt.");
+                connectedClientsCount++;
+                
+                // comparing new IP to known IPs
+                bool isNewDevice = true;
+                for (IPAddress ip : connectedIpAdresses){
+                    if (ip == deviceIP){
+                        isNewDevice = false;
+                        break;
+                    }
                 }
+
+                if(isNewDevice){
+                    Serial.printf("New device %d: MAC: %02X:%02X:%02X:%02X:%02X:%02X, IP: %s\n",
+                    i + 1,
+                    station.mac[0], station.mac[1], station.mac[2],
+                    station.mac[3], station.mac[4], station.mac[5],
+                    deviceIP.toString());
+                    setRgbColor(lastColor, deviceIP.toString());
+                }
+            }
+            // Clear the vector to reset the list of known IP addresses
+            connectedIpAdresses.clear();
+
+            // Save all currently connected device IPs into the vector
+            for (int i = 0; i < adapterStationList.num; i++) {
+                tcpip_adapter_sta_info_t station = adapterStationList.sta[i];
+                IPAddress deviceIP = IPAddress((uint8_t*)&station.ip.addr);
+                connectedIpAdresses.push_back(deviceIP); // Add the device IP to the vector
             }
         }
     };
