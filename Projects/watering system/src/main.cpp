d8e91e63176d620087344bb59d54613d8a6e699d
HelyusHD <you@example.com>
2025-07-24 12:12:17 +0200

    first working pp (parallel processing)

--- Projects/watering system/src/main.cpp
+++ Projects/watering system/src/main.cpp
@@ -1,4 +1,5 @@
 #include <WiFi.h>
+#include <Arduino.h>
 
 // Replace with your network credentials
 const char* ssid     = "ESP32-Access-Point";
@@ -6,6 +7,10 @@ const char* password = "123456789";
 
 // Set web server port number to 80
 WiFiServer server(80);
+
+// create task handeler
+TaskHandle_t Task1;
+void handleClient(void * parameter);
 
 void setup() {
   Serial.begin(115200);
@@ -20,49 +25,71 @@ void setup() {
   Serial.println(IP);
   
   server.begin();
+
+  xTaskCreatePinnedToCore(
+      handleClient, /* Function to implement the task */
+      "Task1", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      0,  /* Priority of the task */
+      &Task1,  /* Task handle. */
+      0); /* Core where the task should run */
+}
+
+void handleClient(void * parameter){
+  for(;;){
+    WiFiClient client = server.available();   // Listen for incoming clients
+
+    if (client) {                             // If a new client connects,
+
+      Serial.print("running on core ");
+      Serial.println(xPortGetCoreID());
+
+      Serial.println("New Client.");          // print a message out in the serial port
+      String currentLine = "";                // make a String to hold incoming data from the client
+      while (client.connected()) {            // loop while the client's connected
+        if (client.available()) {             // if there's bytes to read from the client,
+          char c = client.read();             // read a byte, then
+          Serial.write(c);                    // print it out the serial monitor
+          currentLine += c;
+          if (c == '\n') {                    // if the byte is a newline character
+            // if the current line is blank, you got two newline characters in a row.
+            // that's the end of the client HTTP request, so send a response:
+            if (currentLine.length() == 2) {
+              // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+              // and a content-type so the client knows what's coming, then a blank line:
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println("Connection: close");
+              client.println();
+              
+              // Send your "Hello World" HTML response
+              client.println("<html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>");
+              client.println("<body><h1>Hello World</h1></body></html>");
+              
+              // The HTTP response ends with another blank line
+              client.println();
+              // Break out of the while loop
+              break;
+            } else { // if you got a newline, then clear currentLine
+              currentLine = "";
+            }
+          } else if (c != '\r') {  // if you got anything else but a carriage return character,
+            currentLine += c;      // add it to the end of the currentLine
+          }
+        }
+      }
+      // Close the connection
+      client.stop();
+      Serial.println("Client disconnected.");
+      Serial.println("");
+    }
+    delay(10);
+  }
 }
 
 void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
-
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        currentLine += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 2) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // Send your "Hello World" HTML response
-            client.println("<html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>");
-            client.println("<body><h1>Hello World</h1></body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
+  Serial.print("loop() on core ");
+  Serial.println(xPortGetCoreID());
+  delay(1000);
 }
