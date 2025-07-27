33d03b8adfdee58a9e196c38e0fa0218fbbc88d7
HelyusHD <you@example.com>
2025-07-27 23:07:44 +0200

    added test function for sensor data running in parallel to webside

--- Projects/watering system/include/myServer.h
+++ Projects/watering system/include/myServer.h
@@ -1,19 +1,27 @@
 #include <networkConfig.h>
-#include <Arduino.h>
-#include <WiFi.h>
 
 #ifndef SERVER
 #define SERVER
+    /*
+    is the access point (hosts the network)
+    hosts the webserver
+    will store config data for clients
+    will log all clinets and their data
+    */
     class myServer
     {
     private:
         const char* apPassword = ESP_NETWORK_PASSWORD;
         const char* ssid = ESP_NETWORK_SSID;
-        WiFiServer server;
-        TaskHandle_t Task1;
-        static void handleClient(void * parameter);
+        WiFiServer server; // server from <WiFi.h>
+        TaskHandle_t Task1; // required for dedicated core
+        static void handleUiClient(void * parameter); // hosts the webserver (UI side)
+        int data = 0;
     public:
         myServer(/* args */);
         ~myServer();
+        int apSetup();
+        void loadTestData();
+        void printTestData();
     };
 #endif
