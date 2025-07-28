20217bfdb99b62ee1247755b477bf9c7d5659832
Andre <andre.thullen@gmx.de>
2025-07-28 11:25:28 +0200

    /

--- Projects/solar tracker/src/main.cpp
+++ Projects/solar tracker/src/main.cpp
@@ -1,19 +1,26 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+#include <sunPosition.h>
+
 #define SERVO_1_PIN 8
 #define SERVO_2_PIN 9
 int degree_0_servo_1 = 500;     // in microseconds
-int degree_180_servo_1 = 6500;   // in microseconds
-int degree_0_servo_2 = 550;     // in microseconds
-int degree_180_servo_2 = 6500;   // in microseconds
+int degree_180_servo_1 = 2400;   // in microseconds
+int degree_0_servo_2 = 600;     // in microseconds
+int degree_180_servo_2 = 2400;   // in microseconds
+
 char buffer[10] = "90";
+
 long int clock = 0;
-long int clockMax = 10000;
+long int clockMax = 1000000;
 int counter = 0;
 
+PolarDirection direction;
+Time time;
+
 // put function declarations here:
-void calibratedServo(int degree);
+void calibratedServo(PolarDirection direction);
 
 Servo myServo_1;
 Servo myServo_2;
@@ -23,6 +30,8 @@ void setup() {
   myServo_2.attach(SERVO_2_PIN, degree_0_servo_2, degree_180_servo_2);
   Serial.begin(9600);
   Serial.println(clockMax);
+  time.day = 3;
+  time.hour = 14;
 }  
 
 void loop() {
@@ -34,14 +43,20 @@ void loop() {
     if(angle > 180){
       angle = 360-angle;
     }
-    calibratedServo(angle);
+    direction = sunPosition(time);
+    calibratedServo(direction);
   }
   
 } 
 
-void calibratedServo(int degree){
-  //Serial.readBytesUntil("\n", buffer, 10);  
-  Serial.println(degree);
-  myServo_1.write(degree);
-  myServo_2.write(degree);
+void calibratedServo(PolarDirection direction){
+  //Serial.readBytesUntil("\n", buffer, 10);
+  Serial.println();
+  float a = 0;
+  direction.azimuth = a;
+  direction.elevation = a;
+  Serial.println(direction.azimuth);
+  Serial.println(direction.elevation);
+  myServo_1.write(direction.azimuth);
+  myServo_2.write(direction.elevation);
 }
